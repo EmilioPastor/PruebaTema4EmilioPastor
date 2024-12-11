fa664fa0184b05b82406d0b9a4e9f3521a310a24
EmilioPastor <emiliopastorzurita906@gmail.com>
2024-12-11 17:43:28 +0100

    octavo commit

--- src/app/pacientes-api/page.jsx
+++ src/app/pacientes-api/page.jsx
@@ -1,34 +1,34 @@
 import Link from "next/link";
 import Fallback from "@/components/fallback";
-import Clientes from "@/components/api-clientes";
-import ClienteNuevo from "@/components/api-cliente-nuevo";
+import Pacientes from "@/components/api-pacientes";
+import PacienteNuevo from "@/components/api-paciente-nuevo";
 import { Suspense } from "react";
 
 
 
-async function clientesPage({ searchParams }) {
+async function pacientesPage({ searchParams }) {
     const { query } = await searchParams;
 
-    // Introducimos un retardo artificial
-    // await new Promise(resolve => setTimeout(resolve, 2000))
+    //Introducimos un retardo artificial
+    await new Promise(resolve => setTimeout(resolve, 2000))
 
     return (
         <section className="min-h-screen max-w-[1024px] mx-auto px-10 py-10">
             <Link href="/" className="fixed text-4xl p-2 bg-orange-300 rounded-full">🏠</Link>
 
             <h1 className='py-10 text-3xl text-blue-500 text-center border-b-4 border-b-blue-500'>
-                API REST clientes
+                API REST pacientes
             </h1>
 
-            <Suspense fallback={<Fallback>Nuevo cliente ... </Fallback>}>
-                <ClienteNuevo />
+            <Suspense fallback={<Fallback>Nuevo paciente ... </Fallback>}>
+                <PacienteNuevo />
             </Suspense>
 
             <Suspense fallback={<Fallback>Obteniendo datos ... </Fallback>}>
-                <Clientes query={query || ''} />
+                <Pacientes query={query || ''} />
             </Suspense>
         </section>
     );
 }
 
-export default clientesPage;
+export default pacientesPage;
